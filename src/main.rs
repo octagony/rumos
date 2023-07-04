28e6787e09ea24c654a888d7284f2e394519209b
octagony <octagony@pop-os.localdomain>
2023-07-04 20:39:51 +0300

    [FEAT] Test run of the "set" function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::error::Error;
+
 use clap::{Args, Parser, Subcommand};
 use brightness::Brightness;
 use futures::TryStreamExt;
@@ -5,13 +7,11 @@ use futures::executor;
 mod args;
 use args::{Cli,Commands,SetArgs,};
 
-
-
-fn main() {
+async fn start()->Result<(), Box<dyn Error>>{
     let cli = Cli::parse();
     match &cli.command{
         Commands::Set(percent) => {
-            println!("Set the percentage of brightness to {:?}%", percent.percent);
+           control_brightness(percent).await?;
         }
         Commands::Inc(percent) => {
             println!("Increase the percentage of brightness to {:?}%", percent.percent);
@@ -20,24 +20,30 @@ fn main() {
             println!("Decrease the percentage of brightness to {:?}%", percent.percent);
         }
     }
+    Ok(())
 }
 
+ fn main() {
+    let _ = executor::block_on(start());
+}
 
-async fn show_brightness() -> Result<(), brightness::Error> {
-    brightness::brightness_devices().try_for_each(|dev| async move {
-        let name = dev.device_name().await?;
-        let value = dev.get().await?;
-        println!("Brightness of device {} is {}%", name, value);
-        Ok(())
-    }).await
-} 
-
-async fn control_brightness() -> Result<(), brightness::Error> {
-
+async fn control_brightness(percent:&SetArgs) -> Result<(), brightness::Error> {
     brightness::brightness_devices().try_for_each(|mut dev| async move {
-        let _ = dev.set(90).await?;
+        let _ = dev.set(percent.percent.unwrap() as u32).await?;
         let (device, level) = (dev.device_name().await?, dev.get().await?);
         println!("Brightness of device {} is {}%", device, level);
         Ok(())
     }).await
 }
+
+
+// async fn show_brightness() -> Result<(), brightness::Error> {
+//     brightness::brightness_devices().try_for_each(|dev| async move {
+//         let name = dev.device_name().await?;
+//         let value = dev.get().await?;
+//         println!("Brightness of device {} is {}%", name, value);
+//         Ok(())
+//     }).await
+// } 
+
+
